2dea36b035dd2f581d171589535ee49be7cec7d1
Tanmay-Mirgal <tanmaymirgal26@gmail.com>
2024-12-22 13:25:24 +0530

    Basic frontend setup with pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      This is a Twitter - Clone
-    </h1>
-  )
+import SignUpPage from "./pages/auth/signup/Signup.jsx";
+import LoginPage from "./pages/auth/login/Login.jsx";
+import HomePage from "./pages/home/Home.jsx";
+import { Routes, Route } from "react-router-dom";
+
+
+ export default function App() {
+	return (
+		<div className='flex max-w-6xl mx-auto'>
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/signup' element={<SignUpPage />} />
+				<Route path='/login' element={<LoginPage />} />
+			</Routes>
+		</div>
+	);
 }
